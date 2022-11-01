2a0f2c2c9c59158700b52436ffb15425040f8ba8
Daniel Luis <danniellcontato@gmail.com>
2022-11-01 06:28:10 -0300

    Novo commit

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,9 +1,13 @@
+window.onload = function(){
+    document.getElementById("display").value="45";
+}
+
 function limpar(){
-    document.getElementById('disp').value=''
+    document.getElementById('display').value=''
 }
 
 function addNumber(valor){
     if(valor === 7){
-        document.getElementById('disp').value='7'
+        document.getElementById('display').value += 7
     }
 }
