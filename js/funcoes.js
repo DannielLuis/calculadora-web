c39dea2ea254386a5d8c17cff86fca13db362951
Daniel Luis <danniellcontato@gmail.com>
2023-03-12 06:36:00 -0300

    Testando botoes

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,6 +1,12 @@
+//
+/*
 window.onload = function(){
     document.getElementById("display").value="45";
-}
+}*/
+
+const btn = document.querySelector("[value='8']");
+
+console.log(btn);
 
 function limpar(){
     document.getElementById('display').value=''
@@ -11,3 +17,7 @@ function addNumber(valor){
         document.getElementById('display').value += 7
     }
 }
+
+btn.addEventListener("click", () => {
+    alert("Testando botão 8")
+})
