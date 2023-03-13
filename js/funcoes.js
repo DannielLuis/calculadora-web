ba7fb645a85fddfbbddf02d0df17b108de3244e8
Daniel Luis <danniellcontato@gmail.com>
2023-03-13 02:40:46 -0300

    Adicionando numeros no display

--- js/funcoes.js
+++ js/funcoes.js
@@ -16,24 +16,51 @@ const btn9 = document.querySelector("[value='9']");
 const btn0 = document.querySelector("[value='0']");
 
 //console.log(btn);
+// Adicionar o numero clicado no display
+function addNumero(valor){
+    document.getElementById('display').value += valor
+}
 
 function limpar(){
     document.getElementById('display').value=''
 }
 
+/*
 function addNumber(valor){
     if(valor === 7){
         document.getElementById('display').value += 7
     }
-}
+}*/
 
-btn1.addEventListener("click", () => console.log("Testando botão "+btn1.value));
-btn2.addEventListener("click", () => console.log("Testando botão "+btn2.value));
-btn3.addEventListener("click", () => console.log("Testando botão "+btn3.value));
-btn4.addEventListener("click", () => console.log("Testando botão "+btn4.value));
-btn5.addEventListener("click", () => console.log("Testando botão "+btn5.value));
-btn6.addEventListener("click", () => console.log("Testando botão "+btn6.value));
-btn7.addEventListener("click", () => console.log("Testando botão "+btn7.value));
-btn8.addEventListener("click", () => console.log("Testando botão "+btn8.value));
-btn9.addEventListener("click", () => console.log("Testando botão "+btn9.value));
-btn0.addEventListener("click", () => console.log("Testando botão "+btn0.value));
+
+
+btn1.addEventListener("click", () => {
+    addNumero(btn1.value);
+});
+btn2.addEventListener("click", () => {
+    addNumero(btn2.value);
+});
+btn3.addEventListener("click", () => {
+    addNumero(btn3.value);
+});
+btn4.addEventListener("click", () => {
+    addNumero(btn4.value);
+});
+btn5.addEventListener("click", () => {
+    addNumero(btn5.value);
+});
+btn6.addEventListener("click", () => {
+    addNumero(btn6.value);
+});
+btn7.addEventListener("click", () => {
+    addNumero(btn7.value);
+});
+btn8.addEventListener("click", () => {
+    addNumero(btn8.value);
+});
+btn9.addEventListener("click", () => {
+    addNumero(btn9.value);
+});
+btn0.addEventListener("click", () => {
+    addNumero(btn0.value);
+});
