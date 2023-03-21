38db8e1d457f218838bd5cf0546cb2412ef08096
Daniel Luis <danniellcontato@gmail.com>
2023-03-21 02:51:24 -0300

    Função de soma

--- js/funcoes.js
+++ js/funcoes.js
@@ -21,42 +21,55 @@ const multiplica = document.querySelector("[value='X']");
 const iqual = document.querySelectorAll("button")[4];
 
 //console.log(iqual);
-let num, num1, num2, operador;
+let num, num1, operador = 0;
+var result = 0;
+var num2 = 0;
 var execut = false
 
 // Adicionar o numero clicado no display
 function addNumero(valor){
     if(execut){
         document.getElementById('display').value += valor
-        num2 = document.getElementById('display').value
+        num2 = parseInt(num2+=valor)
     }else{
         document.getElementById('display').value += valor
         num1 = document.getElementById('display').value
+        num1 = parseInt(num1)
     }
+
+    //console.log(num1)
+    //console.log(typeof num1)
+    //console.log(num2)
+    //console.log(typeof num2)
+    //console.log(num3);
 }
 
+// Adiciona o operador no display
 function addOperador(operad){
+    num2 = 0
     operador = operad
     document.getElementById('display').value += operad
     execut = true
+    console.log(operador)
 }
 
+// Processa o resultado
+function resultado(){
+    document.getElementById('display').value='';
+    num1 = num1+num2;
+    document.getElementById('display').value=num1;
+    //return result;
+};
+
+//Limpa o display
 function limpar(){
     document.getElementById('display').value=''
-    num1 = num
-    num2 = num
-    operador = num
+    num1 = 0
+    num2 = 0
+    operador = 0
     execut = false
+    window.console.clear()
 }
-
-/*
-function addNumber(valor){
-    if(valor === 7){
-        document.getElementById('display').value += 7
-    }
-}*/
-
-
 
 btn1.addEventListener("click", () => {
     addNumero(btn1.value);
@@ -95,6 +108,11 @@ mais.addEventListener("click", () => {
 
 iqual.addEventListener("click", () => {
     //addNumero("0000");
-    console.log("O primeiro numero é "+num1)
-    console.log("Operador é "+operador)
+    console.log("O primeiro numero é " + num1);
+    console.log(typeof num1)
+    console.log("Operador é " + operador);
+    console.log("O segundo numero é " + num2);
+    console.log(typeof num2)
+    //console.log("O resultado é " + resultado());
+    resultado();
 });
